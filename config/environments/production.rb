0768d4407982b141f8930dfda70f26368e036cce
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-08 15:35:06 +0100

    Edit config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,11 +24,10 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.compile = true
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
